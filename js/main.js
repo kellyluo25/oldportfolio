58d32e385554a08207116cd96269e692422bdc2a
kellyluo25 <kellyluo25@gmail.com>
2017-12-31 13:02:46 -0500

    updates

--- js/main.js
+++ js/main.js
@@ -1,14 +1,9 @@
-
-// SVG Size
-var width = 700,
-		height = 500;
-
-
-// Load CSV file
-d3.csv("data/wealth-health-2014.csv", function(data){
-
-	// Analyze the dataset in the web console
-	console.log(data);
-	console.log("Countries: " + data.length)
-
+$(window).scroll(function() {
+      if ($(this).scrollTop() > 20){
+      $('.header').addClass('sticky-top smallheader');
+      $('.header').removeClass('fullheader');
+      } else {
+      $('.header').removeClass('sticky-top smallheader');
+      $('.header').addClass('fullheader');
+      }
 });
