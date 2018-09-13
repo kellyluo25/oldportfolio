7b638f804a0bf5d3fa1d8b5bf9f044c404bdcf45
kellyluo25 <kellyluo25@gmail.com>
2018-09-13 16:55:29 -0400

    i have a back button now it's very exciting

--- js/main.js
+++ js/main.js
@@ -8,6 +8,19 @@ $(window).scroll(function() {
       }
 });
 
+function checkOffset() {
+    if($('.page-info').offset().top + $('.page-info').height() >= $('.page-footer').offset().top - 10)
+        $('.page-info').css('position', 'absolute');
+    if($(document).scrollTop() + window.innerHeight < $('.page-footer').offset().top)
+        $('.page-info').css('position', 'fixed'); // restore when you scroll up
+    $('.page-info').text($(document).scrollTop() + window.innerHeight);
+}
+$(document).scroll(function() {
+    checkOffset();
+});
+
+
+
 //$('.entry').height(function(){
 //    return $(this).width();
 //});
